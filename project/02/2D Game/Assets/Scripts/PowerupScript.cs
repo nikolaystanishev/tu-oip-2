71fbeb23e6eac38d0c114319130600ac20d57309
nikolaystanishev <stanishev.nikolay@gmail.com>
2019-05-08 01:24:00 +0300

    Add second part of course project

---
+++ project/02/2D Game/Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupScript : MonoBehaviour
+{
+    HUDScript hud;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+            hud.IncreaseScore(10);
+            Destroy(this.gameObject);
+        }
+    }
+}
